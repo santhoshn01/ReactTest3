60b00f0a4dfd766586653ddc8b66d6c008456dad
Santhosh Natarajan <santhosh_n@CHELLASOFT.COM>
2025-07-28 20:30:11 +0530

    Jenkins commit

--- cypress/config.js
+++ cypress/config.js
@@ -2,6 +2,11 @@ const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: 'http://localhost:3000',
+    supportFile: 'cypress/support/e2e.js',
+    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
+    video: false,
+    screenshotOnRunFailure: true,
     reporter: 'mochawesome',
     reporterOptions: {
       reportDir: 'cypress/reports/mochawesome',
